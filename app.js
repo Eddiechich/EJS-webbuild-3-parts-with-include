85982944ceea70533e72f4c9d18f587f86d054e8
Eddiechich <94399907+Eddiechich@users.noreply.github.com>
2022-07-17 22:31:37 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const PORT = process.env.PORT || 3000
+
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+const path = require("path");
+
+const publicDirectoryPath = path.join(__dirname, "./public");
+
+app.use(express.static(publicDirectoryPath));
+
+const viewsPath = path.join(__dirname,'./views');
+
+app.set('views', viewsPath)
+
+// app.get('', (req, res) => { res.render('index');
+app.get("" , (req, res) => {
+
+res.render("index");
+});
+
+
+app.listen(PORT, () => {
+
+console.log("Server is up on port "+PORT);
+});
